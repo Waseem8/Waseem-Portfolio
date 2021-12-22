2082cc8f41af557232320798a160799d803a7186
M Waseem <wasirana80@gmail.com>
2021-12-22 17:49:29 +0500

    updated

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Web Designer", "Freelancer"],
+        strings: ["Front-End Developer", "React Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Web Designer", "Freelancer"],
+        strings: ["Front-End Developer", "React Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
